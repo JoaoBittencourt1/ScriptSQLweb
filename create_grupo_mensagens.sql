2ba740c3f89b7c00c757978c0ddfba4d926cc023
joao <jvabgo@gmail.com>
2025-06-15 17:44:59 -0300

    remove: removendo script de grupo mensagens

--- create_grupo_mensagens.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE grupo_mensagens (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_grupo INT NOT NULL,
-    id_usuario INT NOT NULL,
-    conteudo TEXT NOT NULL,
-    data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_grupo) REFERENCES grupos(id) ON DELETE CASCADE,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE
-);
