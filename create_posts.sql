1343fa588ef30bcda02307159a55c576e5d6be5f
Eric-Marinho <ericmassenmarinho@gmail.com>
2025-06-15 17:34:57 -0300

    feat: posts agora podem ser redirecionados para grupos

--- create_posts.sql
+++ create_posts.sql
@@ -6,3 +6,11 @@ CREATE TABLE postagens (
     tipo ENUM('texto', 'imagem', 'video') NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE
 );
+
+ALTER TABLE postagens 
+ADD COLUMN id_grupo INT NULL;
+
+
+ALTER TABLE postagens 
+ADD CONSTRAINT fk_posts_grupo 
+FOREIGN KEY (id_grupo) REFERENCES grupos(id);
