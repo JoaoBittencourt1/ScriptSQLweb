69582e7a84d7e525f12c0593e65eacab08b8e009
KurejiV <davivargas374@gmail.com>
2025-06-15 19:14:04 -0300

    Create insercao_comentarios.sql

---
+++ insercao_comentarios.sql
@@ -0,0 +1,11 @@
+INSERT INTO comentarios (id_usuario, id_postagem, conteudo) VALUES
+(2, 1, 'Concordo!'),
+(3, 2, 'Que fofo!'),
+(4, 3, 'Muito legal!'),
+(5, 4, 'Já li, é ótimo!'),
+(6, 5, 'Parabéns pelo talento!'),
+(7, 6, 'Já assisti, muito bom!'),
+(8, 7, 'Disse tudo.'),
+(9, 8, 'Quero também!'),
+(10, 9, 'Top!'),
+(1, 10, 'Você é forte!');
